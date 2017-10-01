4b1769b78a6115da238dd61d48d6402fe3d4b3e8
Dagg3r <phyc4s@gmail.com>
2017-10-01 15:33:41 -0300

    First Commit

---
+++ src/ElPOS/interfaces/mainClass.java
@@ -0,0 +1,18 @@
+package ElPOS.interfaces;
+
+
+
+/**
+ *
+ * @author Phycas
+ */
+public class mainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
